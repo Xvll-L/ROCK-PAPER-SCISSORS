582ffbb5ef9b184cac194cd5deb8f50f977d09eb
Ahmed <ahmedmo501296@gmail.com>
2022-07-03 14:05:21 +0100

    event lister working and results displayed

--- Main.js
+++ Main.js
@@ -36,89 +36,85 @@ function computer(){
 
 
 } 
-aiSelection = computer();
 
 
 
-rock.addEventListener("click",  () => player(rock.textContent));
-paper.addEventListener("click",() => player(paper.textContent) )
-scissors.addEventListener("click", () => player(scissors.textContent) )
 
-function player(text){
+rock.addEventListener("click", function(){playRound("Rock")});
+paper.addEventListener("click", function Paper(){playRound("Paper")})
+scissors.addEventListener("click", function Scissors(){playRound("Scissors") })
 
-    if (text === "rock" || text === "Rock") {
-        return "Rock";
-    } else if (text === "paper" || text === "Paper"){
-        return "Paper";
-    } else if (text === "scissors" || text === "Scissors") {
-        return "Scissors";
+let eventData;
+function player(){
+     
+    if (condition) {
+        
     }
+
+
+    
+
 }
-const test = "Rock"
-console.log(playRound(player, test))
 
-
-function playRound(playerSelection, aiSelection){
+let playerCount = 0;
+let aiCount = 0;
+let  roundCounts = 1;
+function playRound(playerSelection){
+   aiSelection = computer();
    
-    console.log(aiSelection)
+    console.log(playerSelection)
     if(playerSelection === "Rock" && aiSelection === "Scissors" ){
-        return "playerWin"
+        results = "playerWin"
     } else if(playerSelection === "Scissors" && aiSelection === "Rock"){
-        return "aiWin"
+        results = "aiWin"
     } else if(playerSelection === "Scissors" && aiSelection === "Paper"){
-        return "playerWin"
+        results = "playerWin"
     } else if(playerSelection === "Paper" && aiSelection === "Scissors"){
-        return "aiWin"
+        results = "aiWin"
     } else if(playerSelection === "Paper" && aiSelection === "Rock" ){
-        return "playerWin"
+        results = "playerWin"
     } else if(playerSelection === "Rock" && aiSelection === "Paper" ){
-        return "aiWin"
+        results = "aiWin"
     } else if (playerSelection ===   aiSelection){
-        return "tie"
+        results = "tie"
     }
     
+   
+   
     
-      
-
-}
-
-
-
-let i = 0;
-function game(){
-    let playerCount = 0;
-    let aiCount = 0;
-
-    while(i < 50){
-        console.log(i = i + 1)
+        
        
-    /*let promptData = prompt("Input rock paper or scissors");
-
-    if (promptData === "rock" || promptData === "Rock") {
-        promptData = "Rock";
-    } else if (promptData === "paper" || promptData === "Paper"){
-        promptData = "Paper";
-    } else if (promptData === "scissors" || promptData === "Scissors") {
-        promptData = "Scissors";
-    }
- */
-    // let results = playRound(playerSelection,computer())
+    
+     
 
      if (results === "playerWin"){
 
        playerCount = playerCount + 1;
     } else if (results === "aiWin"){
         aiCount = aiCount + 1;
-    }
+    } else 
 
 
     displayPlayer.innerHTML = playerCount;
     displayAi.innerHTML = aiCount
 
-    if (playerCount == 5 || aiCount == 5){
-        break
+    if (playerCount === 5 || aiCount === 5){
+        
+        playerCount = 0
+        aiCount = 0
+       
+      
+        playerMore = prompt("Do you want to player again? Type in yes")
+      
     }
-    }
+    
+
+}
+
+
+
+function game(){
+   
 }
 
 
